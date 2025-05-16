1ce1ad7c9059f94d3192d9cfd476c1302bcb2de4
songtaotao1 <zhaoruike32@gmail.com>
2025-05-16 15:23:06 +0800

    添加路径别名配置

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'], // 允许从 Unsplash 加载图片
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig 
